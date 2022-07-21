f57743f96ca7534ccc655d9794e28b8cf0019112
haojinming <jinming.hao@pingcap.com>
2022-07-21 17:47:26 +0800

    use client checksum
    Signed-off-by: haojinming <jinming.hao@pingcap.com>

--- main.go
+++ main.go
@@ -5,18 +5,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"sync"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/kvproto/pkg/kvrpcpb"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/tikv/client-go/v2/rawkv"
-	"github.com/tikv/client-go/v2/util/codec"
 	"github.com/tikv/migration/br/pkg/conn"
-	"github.com/tikv/migration/br/pkg/gluetikv"
 	"github.com/tikv/migration/br/pkg/pdutil"
-	"github.com/tikv/migration/br/pkg/restore"
 	"github.com/tikv/migration/br/pkg/utils"
 	pd "github.com/tikv/pd/client"
 	"golang.org/x/sync/errgroup"
@@ -76,83 +72,30 @@ func (cfg *Config) ParseFromFlags(flags *pflag.FlagSet) error {
 	return nil
 }
 
-func UpdateChecksum(checksum *rawkv.RawChecksum, another *rawkv.RawChecksum) {
-	checksum.Crc64Xor ^= another.Crc64Xor
-	checksum.TotalKvs += another.TotalKvs
-	checksum.TotalBytes += another.TotalBytes
-}
-
-func ConvertToRawKey(key []byte, apiVersion kvrpcpb.APIVersion) []byte {
-	if apiVersion != kvrpcpb.APIVersion_V2 {
-		return key
-	}
-	rawkey, _, _ := codec.DecodeBytes(key, nil)
-	return rawkey
-}
-
-func GetChecksum(ctx context.Context, pdAddr string, startKey, endKey []byte) (rawkv.RawChecksum, error) {
+func GetClusterAPIVersion(ctx context.Context, pdAddr string) (kvrpcpb.APIVersion, error) {
 	pdCtrl, err := pdutil.NewPdController(ctx, pdAddr, nil, pd.SecurityOption{})
 	if err != nil {
 		fmt.Printf("new pd client fail, %v.\n", err)
-		return rawkv.RawChecksum{}, err
+		return kvrpcpb.APIVersion_V1, err
 	}
-	pdClient := pdCtrl.GetPDClient()
-	apiVersion, err := conn.GetTiKVApiVersion(ctx, pdClient, nil)
+	apiVersion, err := conn.GetTiKVApiVersion(ctx, pdCtrl.GetPDClient(), nil)
 	if err != nil {
-		fmt.Printf("get api version, %v.\n", err)
-		return rawkv.RawChecksum{}, err
+		fmt.Printf("get api version fail, %v.\n", err)
+		return kvrpcpb.APIVersion_V1, err
 	}
+	return apiVersion, nil
+}
 
-	splitClient := restore.NewSplitClient(pdClient, nil, true)
-	regionInfo, err := restore.PaginateScanRegion(ctx, splitClient, startKey, endKey, 1024)
-	if err != nil {
-		fmt.Printf("get regions fail, %v.\n", err)
-		return rawkv.RawChecksum{}, err
-	}
-	keyRanges := []*utils.KeyRange{}
-	for _, region := range regionInfo {
-		keyRange := utils.KeyRange{
-			Start: ConvertToRawKey(region.Region.StartKey, apiVersion),
-			End:   ConvertToRawKey(region.Region.EndKey, apiVersion),
-		}
-		keyRanges = append(keyRanges, &keyRange)
-	}
+func GetChecksum(ctx context.Context, pdAddr string, apiVersion kvrpcpb.APIVersion, startKey, endKey []byte) (rawkv.RawChecksum, error) {
 	rawkvClient, err := rawkv.NewClientWithOpts(ctx, strings.Split(pdAddr, ","), rawkv.WithAPIVersion(apiVersion))
 	if err != nil {
 		fmt.Printf("new rawkv client fails, %v.\n", err)
 		return rawkv.RawChecksum{}, err
 	}
-	glue := new(gluetikv.Glue)
-	updateCh := glue.StartProgress(ctx, "Raw Checksum", int64(len(keyRanges)), false)
-	fmt.Println("total ranges", len(keyRanges))
-	progressCallBack := func() {
-		// fmt.Println("checksum finish one")
-		updateCh.Inc()
-	}
-	storageChecksum := rawkv.RawChecksum{}
-	lock := sync.Mutex{}
-	workerPool := utils.NewWorkerPool(CHECKSUM_CONCURRENCY, "Ranges")
-	eg, _ := errgroup.WithContext(ctx)
-	for _, r := range keyRanges {
-		keyRange := r // copy to another variable in case it's overwritten
-		workerPool.ApplyOnErrorGroup(eg, func() error {
-			ret, err := rawkvClient.Checksum(ctx, keyRange.Start, keyRange.End)
-			if err != nil {
-				return err
-			}
-			lock.Lock()
-			UpdateChecksum(&storageChecksum, &ret)
-			// fmt.Println("checksum finish one here")
-			progressCallBack()
-			lock.Unlock()
-			return nil
-		})
-	}
-	err = eg.Wait()
+	storageChecksum, err := rawkvClient.Checksum(ctx, startKey, endKey)
 	if err != nil {
 		return rawkv.RawChecksum{}, err
 	}
-	updateCh.Close()
 	return storageChecksum, nil
 }
 
@@ -164,14 +107,41 @@ func runRawKvChecksum(cmd *cobra.Command, name string) error {
 		return err
 	}
 	ctx := context.Background()
-	srcChecksum, err := GetChecksum(ctx, cfg.SrcPD, cfg.StartKey, cfg.EndKey)
+	srcAPIVersion, err := GetClusterAPIVersion(ctx, cfg.SrcPD)
 	if err != nil {
-		fmt.Printf("calc src cluster checksum fails, %v.\n", err)
 		return err
 	}
-	dstChecksum, err := GetChecksum(ctx, cfg.DstPD, cfg.StartKey, cfg.EndKey)
+	dstAPIVersion, err := GetClusterAPIVersion(ctx, cfg.DstPD)
 	if err != nil {
-		fmt.Printf("calc dst cluster checksum fails, %v.\n", err)
+		return err
+	}
+	if srcAPIVersion != dstAPIVersion {
+		fmt.Printf("Different api version between src:%v and dst:%v.\n", srcAPIVersion, dstAPIVersion)
+		return errors.Errorf("Different api version.")
+	}
+	srcChecksum := rawkv.RawChecksum{}
+	dstChecksum := rawkv.RawChecksum{}
+	errGroup := new(errgroup.Group)
+	errGroup.Go(func() error {
+		srcChecksum, err = GetChecksum(ctx, cfg.SrcPD, srcAPIVersion, cfg.StartKey, cfg.EndKey)
+		if err != nil {
+			fmt.Printf("calc src cluster checksum fails, %v.\n", err)
+			return err
+		}
+		fmt.Printf("src cluster checksum, %v.\n", srcChecksum)
+		return nil
+	})
+	errGroup.Go(func() error {
+		dstChecksum, err = GetChecksum(ctx, cfg.DstPD, dstAPIVersion, cfg.StartKey, cfg.EndKey)
+		if err != nil {
+			fmt.Printf("calc dst cluster checksum fails, %v.\n", err)
+			return err
+		}
+		fmt.Printf("dst cluster checksum, %v.\n", dstChecksum)
+		return nil
+	})
+	err = errGroup.Wait()
+	if err != nil {
 		return err
 	}
 	if srcChecksum != dstChecksum {
@@ -184,8 +154,8 @@ func runRawKvChecksum(cmd *cobra.Command, name string) error {
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:              "rawkv-data-check",
-		Short:            "rawkv-data-check is a data consistency check tool for tikv raw mode.",
+		Use:              "tikv-data-compare",
+		Short:            "tikv-data-compare is a data consistency check tool for tikv raw mode.",
 		TraverseChildren: true,
 		SilenceUsage:     true,
 		Args:             cobra.NoArgs,
